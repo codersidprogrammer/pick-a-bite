fa1111073d9a1563bada7539683e15667c791494
dimaseditiya <dimaseditiya.energi@gmail.com>
2025-04-10 06:20:50 +0700

    [update] all working on history needs and haptic

--- pick-a-bite/PickABite/View/RouletteDiaryPage/HistoryPageView.swift
+++ pick-a-bite/PickABite/View/RouletteDiaryPage/HistoryPageView.swift
@@ -5,6 +5,7 @@ struct HistoryPageView: View {
     @Environment(\.modelContext) private var modelContext
     @State private var isPresented: Bool = false
     @State var histories: [UserHistoryModel] = []
+    @State var groupedHistories: [String: [UserHistoryModel]] = [:]
     
     var body: some View {
         ZStack {
@@ -17,16 +18,27 @@ struct HistoryPageView: View {
                 }
             } else {
                 List {
-                    ForEach(Array(histories.enumerated()), id: \.offset) { index, value in
-                        DailyCard(isPresented: $isPresented, dataModel: value)
-                            .listRowBackground(Color.clear)
+                    ForEach(Array(groupedHistories.sorted(by: { $0.key > $1.key }).enumerated()), id: \.offset) { index, value in
+                        Section(header: Text(value.key)
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                            .padding(.top)) {
+                            
+                            ForEach(value.value, id: \.id) { history in
+                                DailyCard(isPresented: $isPresented, dataModel: history)
+                                    .listRowBackground(Color.clear)
+                            }
+                        }
                     }
+
                     .background(Color.clear)
                 }
                 .listStyle(PlainListStyle())
                 .background(Color.clear)
                 .sheet(isPresented: $isPresented) {
                     ModalView(isPresented: $isPresented, dataModel: $histories[0])
+                        .padding(.horizontal, Sizing.md)
+                        .padding(.vertical, Sizing.sm)
                         .presentationDetents([.fraction(0.6)])
                         .presentationDragIndicator(.visible)
                 }
@@ -34,7 +46,18 @@ struct HistoryPageView: View {
         }
         .onAppear() {
             do {
-                histories = try modelContext.fetch(FetchDescriptor<UserHistoryModel>())
+                let descriptor = FetchDescriptor<UserHistoryModel>(
+                    sortBy: [SortDescriptor<UserHistoryModel>(\.createdAt, order: .reverse)]
+                )
+                let formatter = DateFormatter()
+                formatter.dateFormat = "EEEE, dd MMMM yyyy" // adjust if you want more readable format
+                
+                
+                histories = try modelContext.fetch(descriptor)
+                
+                groupedHistories = Dictionary(grouping: histories) { item in
+                    formatter.string(from: item.createdAt)
+                }
             } catch let error {
                 print(error.localizedDescription)
             }
